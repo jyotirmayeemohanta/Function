f31e1084bf779ec9708f27aa5b1ecad7279ee107
jyotirmayeemohanta <97024151+jyotirmayeemohanta@users.noreply.github.com>
2022-01-20 15:42:13 +0530

    Add files via upload

---
+++ Inner function.py
@@ -0,0 +1,15 @@
+def f1():
+   s = "I Love Navgurukul"
+   def f2():
+       print(s)
+   f2()
+f1()
+
+
+def f1():
+   s = "I Love Navgurukul"
+   def f2():
+       print(s)
+   f2()
+   print("I LOVE JYOTI")
+f1()

f4f78bffc459ce87d7685861d996ce226d9ea03a
jyotirmayeemohanta <97024151+jyotirmayeemohanta@users.noreply.github.com>
2022-08-29 19:51:00 +0530

    Add files via upload

---
+++ Inner function1.py
@@ -0,0 +1,17 @@
+def first_function():
+    s = 'I love India'
+    def second_function():
+        print(s)     
+    second_function()
+first_function()
+
+
+
+def first_function():
+    s = 'I love India'
+    def second_function():
+        print(s)     
+    second_function()
+    print("I LOVE KEDARNATH")
+first_function()
+
